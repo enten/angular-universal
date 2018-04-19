a4f6af6ea35310b156818654080425e54e77dbdb
Steven Enten <steven@enten.fr>
2018-04-19 18:38:06 +0200

    enable hmr on browser side

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, NgModuleRef, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { createNewHosts } from '@angularclass/hmr';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +10,46 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+function bootstrap (boot: () => Promise<NgModuleRef<any>>) {
+  if (module.hot) {
+    return bootstrapWithHmr(boot);
+  }
+
+  return boot();
+}
+
+function bootstrapWithHmr (boot: () => Promise<NgModuleRef<any>>) {
+  let ngModule: NgModuleRef<any>;
+
+  module.hot.accept();
+
+  const booting = boot().then((mod) => {
+    ngModule = mod;
+
+    return mod;
+  });
+
+  module.hot.dispose(() => {
+    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+    const elements = appRef.components.map(c => c.location.nativeElement);
+    const makeVisible = createNewHosts(elements);
+
+    ngModule.destroy();
+
+    makeVisible();
+  });
+
+  return booting;
+}
+
+function main () {
+  bootstrap(() => platformBrowserDynamic().bootstrapModule(AppModule))
+    .then(ngModule => console.log('AppModule boostraped!'))
+    .catch(console.error);
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', main);
+} else {
+  main();
+}
