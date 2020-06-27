39e837d736a6c9546f0d58113d9dd1d9544a7601
Steven Enten <steven@enten.fr>
2020-06-27 22:17:42 +0200

    feat: add service worker

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
-  imports: [ AppModule ],
+  imports: [ AppModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
   bootstrap: [ AppComponent ],
 })
 export class AppBrowserModule { }
